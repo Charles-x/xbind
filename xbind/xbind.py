cfd4e33037d28087f7cd5deb784da1a4e63f8575
charles.xiao <charles-xiao@live.com>
2017-11-26 23:59:17 +0800

    V0.2

--- xbind/xbind.py
+++ xbind/xbind.py
@@ -4,6 +4,9 @@
 
 import dns.update
 import dns.query
+import dns.zone
+import dns.resolver
+import dns.reversename
 import dns.tsigkeyring
 import re
 import json
@@ -11,54 +14,6 @@ import json
 
 ##dtwh.com
 ##192.192.in-addr.arpa
-
-class xbinddns(object):
-    def __init__(self,zoneA,zonePTR,dnsserver,tsig_key_name,tsig_key):
-        self.dnsserver = dnsserver
-        self.zoneA = zoneA
-        self.zonePTR = zonePTR
-        self.keyring = dns.tsigkeyring.from_text({tsig_key_name: tsig_key})
-        self.updateA = dns.update.Update(self.zoneA,keyring=self.keyring)
-        self.updatePTR = dns.update.Update(self.zonePTR,keyring=self.keyring)
-
-    def dns_add(self,record,ip,FQDN,TTL):
-        if record =='A':
-            self.updateA.add(FQDN,TTL,record,ip)
-        elif record =='PTR':
-            iprecord = xbindtool.ip_antitone(ip)
-            self.updatePTR.add(iprecord,TTL,record,FQDN)
-
-    def dns_delete(self):
-        pass
-
-    def dns_replace(self):
-        pass
-
-    def dns_query(self,ip = None,hostname = None):
-        if ip & hostname ==None:
-            handler = dns.resolver.query(self.zoneA, 'A')
-            print handler
-
-    def dns_commit(self,update):
-        response = dns.query.tcp(update, self.dnsserver)
-        return response
-
-    def A_PTR_all(self,ip,FQDN,TTL=36500):
-        iprecord = xbindtool.ip_antitone(ip)
-        if '.'.join(FQDN.split('.')[1:]) == self.zoneA:
-            hostname = FQDN.split('.')[0]
-            FQDN = FQDN+'.'+self.zoneA+'.'
-        else:
-            hostname = FQDN.split('.')[0]
-            FQDN = FQDN+'.'+self.zoneA+'.'
-        self.updateA.add(hostname,TTL,'A',ip)
-        self.updatePTR.add(iprecord,TTL,'PTR',FQDN)
-        A_response = self.dns_commit(self.updateA)
-        PTR_response = self.dns_commit(self.updatePTR)
-        print "A####################:\n",A_response ,"\nPTR##################:\n",PTR_response
-
-
-
 
 class xbindlocal:
     @staticmethod
@@ -137,7 +92,7 @@ class xbindverify():
 
 
 class xbind():
-    def dataget_all(self,ipdata,FQDNdata,TTL=36500):
+    def dataget_all(self,ipdata,FQDNdata,TTL=604800):
         self.TTL = TTL
         #verify data
         self.ip = xbindverify.is_ip(ipdata)
@@ -151,18 +106,23 @@ class xbind():
         self.PTR_domain = xbindtool.ip_PTRname(self.ip)+".in-addr.arpa"
         A_domain_info = xbindtool.choosezone(self.A_domain)
         PTR_domain_info = xbindtool.choosezone(self.PTR_domain)
+
+        #get keyring info
         A_domain_keyname = A_domain_info[0][-1]
         PTR_domain_keyname = PTR_domain_info[0][-1]
 
         ##user keyname get key
         A_domain_key = xbindtool.choosekey(A_domain_keyname)[0][-1]
         PTR_domain_key = xbindtool.choosekey(PTR_domain_keyname)[0][-1]
-        if A_domain_key == PTR_domain_key and A_domain_keyname == PTR_domain_keyname:
-            self.tsig_key_name = A_domain_keyname ##########
-            self.tsig_key = A_domain_key          ##########
-        else:
-            #TODO:diff key to use dns
-            pass
+
+        self.tsig_key_name_A = A_domain_keyname ##########
+        self.tsig_key_A = A_domain_key          ##########
+        self.tsig_key_name_PTR = PTR_domain_keyname ##########
+        self.tsig_key_PTR = PTR_domain_key          ##########
+        ######## make keyring
+        self.keyring_A = dns.tsigkeyring.from_text({self.tsig_key_name_A: self.tsig_key_A})
+        self.keyring_PTR = dns.tsigkeyring.from_text({self.tsig_key_name_PTR: self.tsig_key_PTR})
+
 
         ###zoneA data
         self.hostname = domainall[0]
@@ -181,9 +141,10 @@ class xbind():
         print "zone A     ",self.zoneA
         self.zonePTR = self.PTR_domain+"."
         print "PTR_domain:   "+self.PTR_domain
-        self.keyring = dns.tsigkeyring.from_text({self.tsig_key_name: self.tsig_key})
-        self.updateA = dns.update.Update(self.zoneA,keyring=self.keyring)
-        self.updatePTR = dns.update.Update(self.zonePTR,keyring=self.keyring)
+        if self.keyring_A != None:
+            self.updateA = dns.update.Update(self.zoneA,keyring=self.keyring_A)
+        if self.keyring_PTR !=None:
+            self.updatePTR = dns.update.Update(self.zonePTR,keyring=self.keyring_PTR)
         print "dnsserver:   "+self.dnsserver
         print "zoneA:   "+self.zoneA
         print "zonePTR:   "+self.zonePTR
@@ -199,16 +160,65 @@ class xbind():
             self.updateA.add(self.hostname, self.TTL, "A", self.ip)
             self.updatePTR.add(self.PRTip_d, self.TTL, "PTR", self.PTRdomain_d)
 
-    def delete(self):
-        pass
+    def delete(self,record):
+        if record =='A':
+            self.updateA.delete(self.hostname)
+        elif record =='PTR':
+            self.updatePTR.delete(self.PRTip_d)
+        elif record =="all":
+            self.updateA.delete(self.hostname)
+            self.updatePTR.delete(self.PRTip_d)
 
     def update(self):
         pass
 
-    def read(self,ip = None,hostname = None):
-        if ip & hostname ==None:
-            handler = dns.resolver.query(self.zoneA, 'A')
-            print handler
+    def read(self,ip = None,FQDN = None):
+        if ip == None and FQDN == None:
+            zones = xbindlocal.zoneparse()
+            data = []
+            for i in zones:
+                data.append(i[0])
+            return json.dumps({"data": data})
+
+        elif ip and FQDN != None:
+            #TODO:use ip and FQDN to verify the relevance
+            pass
+
+        elif ip == None and FQDN != None:
+            if FQDN[-1] == ".":
+                # show the all record from the domain
+                domain = FQDN
+                z = dns.zone.from_xfr(dns.query.xfr(self.dnsserver, domain))
+                names = z.nodes.keys()
+                data = []
+                for n in names:
+                    FQDNrecord = "{}.{}".format(n, domain)
+                    record = z[n].to_text(n).encode("utf-8").split(" ")
+                    if len(record) == 5:
+                        data.append({"ip": record[-1], "FQDN": FQDNrecord})
+                return json.dumps({"data":data})
+
+            else:
+                FQDNr = dns.name.from_text(FQDN)
+                r = dns.resolver.Resolver()
+                r.nameservers = [self.dnsserver]
+                query = r.query(FQDNr, "A")
+                response = query.response
+                data = response.answer[0].to_text().split()[-1]
+                return json.dumps({"FQDN": FQDN, "ip": data})
+
+
+        elif ip !=None and FQDN ==None:
+            addr = dns.reversename.from_address(ip)
+            r = dns.resolver.Resolver()
+            r.nameservers = [self.dnsserver]
+            query = r.query(addr, "PTR")
+            response = query.response
+            data = response.answer[0].to_text().split()[-1]
+            name = data[:-1]
+            return json.dumps({"FQDN":name,"ip":ip})
+
+
 
     def commit(self,record):
         if record =='A':
@@ -221,11 +231,3 @@ class xbind():
             response1 = dns.query.tcp(self.updateA, self.dnsserver)
             response2 = dns.query.tcp(self.updatePTR, self.dnsserver)
             return response1,response2
-
-if __name__ == '__main__':
-    bind_handle = xbind()
-    bind_handle.dataget_all("192.192.3.120","xiao3.dtwh.com")
-    bind_handle.init(dnsserver="172.16.137.11")
-    bind_handle.create("all")
-    print bind_handle.commit("all")
-
--- xbind/xbindapi.py
+++ xbind/xbindapi.py
@@ -9,29 +9,17 @@ import xbind
 ##dtwh.com
 ##192.192.in-addr.arpa
 
-zoneA = "dtwh.com"
-zonePTR = "192.192.in-addr.arpa"
-dnsserver = "172.16.137.11"
-tsig_key_name = "key01"
-tsig_key = "OPJEiEP3oqxxnOGCcyezuQ=="
-tsig_algorithm = "hmac-md5"
-
 app = Flask(__name__)
 
 @app.route('/dns', methods=['GET','DELETE','POST'])
 def get_tasks():
-    handler = xbind.xbinddns(zoneA, zonePTR, dnsserver, tsig_key_name, tsig_key)
     if request.method == 'GET':
-        hostname = request.args.get('hostname')
-        ip = request.args.get('ip')
-        if hostname and ip !=None:
-            handler.A_PTR_all(ip,hostname)
-        elif hostname != None:
-            handler.dns_query(hostname)
-        elif ip != None:
-            handler.dns_query(ip)
-        else:
-            handler.dns_query(zoneA)
+        ip = request.args.get('ip',None)
+        FQDN = request.args.get('FQDN',None)
+        handler_dns = xbind.xbind()
+        data = handler_dns.read(ip,FQDN)
+        return data
+
 
     elif request.method == 'POST':
         rdata = request.get_json()
@@ -44,7 +32,7 @@ def get_tasks():
             if ip and FQDN:
                 handler_dns = xbind.xbind()
                 handler_dns.dataget_all(ip,FQDN)
-                handler_dns.init(dnsserver="172.16.137.11")
+                handler_dns.init()
                 handler_dns.create("all")
                 result = handler_dns.commit("all")
                 for i in result:
@@ -59,13 +47,25 @@ def get_tasks():
 
 
     elif request.method == 'DELETE':
-        print "delete"
-        hostname = request.args.get('hostname')
-        ip = request.args.get('ip')
-        if hostname and ip != None:
-            print hostname,ip
-
-    return 'ok'
+        rdata = request.get_json()
+        if rdata != None:
+            data = rdata
+            ip = data.get("ip", None)
+            FQDN = data.get("FQDN", None)
+            if ip and FQDN:
+                handler_dns = xbind.xbind()
+                handler_dns.dataget_all(ip, FQDN)
+                handler_dns.init()
+                handler_dns.delete("all")
+                result = handler_dns.commit("all")
+                for i in result:
+                    print i
+            sdata = {"request": "success", "data": "ok!", "code": "6"}
+            return json.dumps(sdata)
+        else:
+            print "else"
+            edata = {"request": "success", "data": "data type error!", "code": "0"}
+            return json.dumps(edata)
 
 # class xbinderror:
 #     def __init__(self):
